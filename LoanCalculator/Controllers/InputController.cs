faf7c4d98ad8fb672463926b2b89515523f02aaf
Swathi Kapu <swathipentapalli@gmail.com>
2019-07-07 16:43:03 +0100

    Showing loan amount and number of years in summary

---
+++ LoanCalculator/Controllers/InputController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoanCalculator.Models;
+
+namespace LoanCalculator.Controllers
+{
+    public class InputController : Controller
+    {
+        // GET: Input
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(double LoanAmount, int NumYears)
+        {
+            LoanCalculatorModel loanCalculator = new LoanCalculatorModel();
+            loanCalculator.LoanAmount = LoanAmount;
+            loanCalculator.NumYears = NumYears;
+            return RedirectToAction("Summary", new { LoanAmount = LoanAmount, NumYears = NumYears });
+        }
+
+        public ActionResult Summary(double LoanAmount, int NumYears)
+        {
+            ViewBag.LoanAmount = LoanAmount;
+            ViewBag.NumYears = NumYears;
+            return View(ViewBag);
+        }
+    }
+}
---
+++ LoanCalculator/Models/LoanCalculatorModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LoanCalculator.Models
+{
+    public class LoanCalculatorModel
+    {
+        public double LoanAmount { get; set; }
+        public int NumYears { get; set; }
+        LoanInterestDBEntities db = new LoanInterestDBEntities();
+
+        public decimal calculateInterest()
+        {
+            decimal amountInterestRate = db.AmountInterestRates.Single(
+                air => LoanAmount > decimal.ToDouble(air.MinAmount) && LoanAmount <= decimal.ToDouble(air.MaxAmount)
+            ).InterestRate;
+
+            decimal yearInterestRate = db.YearInterestRates.Single(
+                yir => NumYears >yir.MinYears && NumYears <= yir.MaxYears
+                ).InterestRate;
+
+            return amountInterestRate + yearInterestRate;
+        }
+    }
+}
