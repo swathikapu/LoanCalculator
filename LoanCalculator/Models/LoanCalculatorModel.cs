a7abe893ea5610dcadfd780baae0ac56861cfc9e
Swathi Kapu <swathipentapalli@gmail.com>
2019-07-08 22:50:45 +0100

    Optimised function by calling them only once

--- LoanCalculator/Models/LoanCalculatorModel.cs
+++ LoanCalculator/Models/LoanCalculatorModel.cs
@@ -12,10 +12,14 @@ namespace LoanCalculator.Models
         public double LoanAmount { get; set; } 
         [Required]
         public int NumYears { get; set; }
+        private double? _InterestRate = null;
+        private double? _MonthlyPayment = null;
         LoanInterestDBEntities db = new LoanInterestDBEntities();
 
         public double calculateInterest()
         {
+            if (_InterestRate != null)
+                return (double) _InterestRate;
             // Converting AmountInterestRates table to AsEnumerable as 
             // Linq is throwing an exception when converting decimal to double.
             decimal amountInterestRate = db.AmountInterestRates.AsEnumerable().Single(
@@ -25,18 +29,19 @@ namespace LoanCalculator.Models
             decimal yearInterestRate = db.YearInterestRates.Single(
                 yir => NumYears >yir.MinYears && NumYears <= yir.MaxYears
             ).InterestRate;
-            return Convert.ToDouble((decimal) (amountInterestRate + yearInterestRate));
+            _InterestRate = Convert.ToDouble((decimal)(amountInterestRate + yearInterestRate));
+            return (double) _InterestRate;
         }
-
 
         public double getMonthlyPayment()
         {
+            if (_MonthlyPayment != null)
+                return (double)_MonthlyPayment;
             double numOfMonths = Convert.ToDouble(NumYears * 12);
             double monthlyInterestRate = calculateInterest() / 1200;
             double power = Math.Pow(1 + monthlyInterestRate, numOfMonths);
-            double payment = Math.Round(LoanAmount * monthlyInterestRate/ (1 - 1/ power), 2);
-            return payment;
-
+            _MonthlyPayment = Math.Round(LoanAmount * monthlyInterestRate/ (1 - 1/ power), 2);
+            return (double) _MonthlyPayment;
         }
 
         public double getTotalPayment()
